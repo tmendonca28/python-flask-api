83859f8ee2777b69351d5490de76d1ae323bf863
tmendonca28 <tmendonca28@gmail.com>
2020-10-06 17:31:41 +0300

    Adds persistence and patch
    Adds persistence using SQLAlchemy
    Adds patch and delete methods
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

--- main.py
+++ main.py
@@ -1,31 +1,82 @@
 from flask import Flask, request
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Video(name={name}, views={views}, likes={likes})"
+
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
 video_put_args.add_argument("views", type=int, help="Views of the video is required", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes on the video is required", required=True)
 
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name", type=str, help="Name of the video is required")
+video_update_args.add_argument("views", type=int, help="Views of the video is required")
+video_update_args.add_argument("likes", type=int, help="Likes on the video is required")
 
-videos = {}
 
-
-def abort_if_video_not_exist(video_id):
-    if video_id not in videos:
-        abort(404, message="Video id is not valid...")
+resource_fields = {
+    "id": fields.Integer,
+    "name": fields.String,
+    "views": fields.Integer,
+    "likes": fields.Integer
+}
 
 class Video(Resource):
+    @marshal_with(resource_fields)
     def get(self, video_id):
-        abort_if_video_not_exist(video_id)
-        return videos[video_id]
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Could not find video with said id...")
+        return result
 
+    @marshal_with(resource_fields)
     def put(self, video_id):
         args=video_put_args.parse_args()
-        videos[video_id] = args
-        return videos[video_id], 201
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if result:
+            abort(409, message="Video id taken...")
+        video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201
+
+    @marshal_with(resource_fields)
+    def patch(self, video_id):
+        args = video_update_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Video does not exist, cannot update")
+        if args["name"]:
+            result.name = args['name']
+        if args["views"]:
+            result.name = args['views']
+        if args["likes"]:
+            result.name = args['likes']
+
+        db.session.commit()
+
+        return result
+
+
+    def delete(self, video_id):
+        del videos[video_id]
+        return '', 204
+
+
 
 api.add_resource(Video, "/video/<int:video_id>")
 
--- test.py
+++ test.py
@@ -2,9 +2,14 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-# response = requests.put(BASE + "video/1", {"likes":10, "name":"Anthony", "views":28})
-# print(response.json())
+# data = [{"likes":10, "name":"Anthony", "views":28},
+#         {"likes":78, "name":"Miguel", "views":100},
+#         {"likes":226000, "name":"Melvin", "views":300000}]
 
-# input()
-response = requests.get(BASE + "video/6")
+
+# for i in range(len(data)):
+#     response = requests.put(BASE + "video/" + str(i), data[i])
+#     print("[*] ",response.json())
+
+response = requests.patch(BASE + "video/2", {"views": 999})
 print(response.json())
